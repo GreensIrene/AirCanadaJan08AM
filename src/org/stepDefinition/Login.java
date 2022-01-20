d2757510850acc48d64df7011c1bba32a26286ec
GreensIrene <albeenairene28@gmail.com>
2022-01-20 08:40:35 +0530

    Username Functionality

--- src/org/stepDefinition/Login.java
+++ src/org/stepDefinition/Login.java
@@ -2,4 +2,11 @@ package org.stepDefinition;
 
 public class Login {
 
+	public static void userName() {
+		System.out.println("UserName");
+	}
+	
+	public static void main(String[] args) {
+		userName();
+	}
 }
